46f2d45aa174dde31015a20c506114294bb59a85
smzw <zhaoyh603@163.com>
2016-10-26 17:11:23 +0800

    1.1
    增加主键

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('GBK')
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle://gdmid:1@127.0.0.1:1521/orcl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle://cw_exchg160715:1@10.10.3.143:1521/asupp11'
 app.config['TESTING'] = True
 db = SQLAlchemy(app)
 
@@ -16,6 +16,7 @@ class User_table():
     table_name = ''
     table_comments=''
     cols=[]
+    cons=[]
     
 
     def __init__(self, table_name, table_comments):
@@ -29,9 +30,11 @@ class User_table():
 def index():
     name={}
 
-    sql_tab=text("select table_name,comments from user_tab_comments where table_type='TABLE'")
+    sql_tab=text("select table_name,decode(comments,null,(select res_na from as_lang_trans a \
+       where a.res_id=t.table_name),comments) comments \
+       from user_tab_comments t where table_type='TABLE' order by table_name")
     sql_cols=text(
-	'''
+	     '''
        select t.table_name,
        t.comments table_comments,
        c.column_id,
@@ -40,7 +43,8 @@ def index():
        decode(c.data_type, 'NUMBER', c.data_length - c.DATA_SCALE,c.data_length) ||
        decode(c.data_type, 'NUMBER', ',') || c.DATA_SCALE || ')' data_type,
        c.nullable,
-       cc.comments col_comments
+       c.DATA_DEFAULT,
+       decode(cc.comments,null,(select res_na from as_lang_trans a where a.res_id=cc.column_name),cc.comments) col_comments
        FROM user_tab_cols c, user_col_comments cc, user_tab_comments t
        WHERE c.table_name = t.table_name
        and c.table_name = cc.table_name
@@ -50,16 +54,32 @@ def index():
        and t.table_name=:table
        order by t.table_name, c.column_id
        ''')
-
+    sql_cons=text(
+      "select c.constraint_name,\
+       decode(c.constraint_type,\
+              'P',\
+              'Primary Key',\
+              'R',\
+              'Referential AKA Foreign Key',\
+              'U',\
+              'Unique Key') constraint_type,\
+       cc.column_name\
+       from user_constraints c, user_cons_columns cc\
+       where c.owner = cc.owner\
+         AND c.constraint_name = cc.constraint_name\
+         AND c.table_name = cc.table_name\
+         and c.constraint_type in ('P', 'R', 'U')\
+         AND C.table_name = :table\
+       ORDER BY constraint_name, cc.column_name\
+      ")  
     tabs= db.session.execute(sql_tab).fetchall() 
     tables=[]
     for tab in tabs:
         t=User_table(tab.table_name,tab.comments)
-        print t
         t.cols = db.session.execute(sql_cols,{'table':tab.table_name}).fetchall() 
+        t.cons = db.session.execute(sql_cons,{'table':tab.table_name}).fetchall() 
+        print t
         tables.append(t)
-    print tables
-
     return render_template('datadict.html', tabs=tables)
 
 if __name__ == '__main__':
